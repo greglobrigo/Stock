7d047770a906cb8c8b9552a1b9086604d0bf15fd
LAPTOP-CTUKF4T0\Ahyan <bryanlontoc06@gmail.com>
2021-12-28 14:37:59 +0800

    - schema

--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,12 @@ ActiveRecord::Schema.define(version: 2021_12_27_172034) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "full_name"
     t.string "status", default: "pending"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.decimal "balance", default: "0.0"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
