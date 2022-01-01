a416f6194f1a4ba9783eee63bc2582e05e44a880
greglobrigo <golivefun@gmail.com>
2022-01-01 20:16:42 +0800

    added project requirements

--- app/controllers/profile_controller.rb
+++
@@ -1,9 +0,0 @@
-class ProfileController < ApplicationController
-    before_action :authenticate_user! do
-        redirect_to unapproved_path unless current_user.status == 'approved'
-    end
-
-    def index
-        @stocks = current_user.stocks
-    end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_27_172034) do
+ActiveRecord::Schema.define(version: 2021_12_30_194134) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 2021_12_27_172034) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
     t.integer "quantity", default: 0
+    t.integer "total_owned", default: 0
+    t.string "transaction_type"
   end
 
   create_table "users", force: :cascade do |t|
