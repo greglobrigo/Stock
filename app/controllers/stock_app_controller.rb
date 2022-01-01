ea0bd673da5e43c2ee1f63aabb36adfee0304885
LAPTOP-CTUKF4T0\Ahyan <bryanlontoc06@gmail.com>
2022-01-02 03:40:12 +0800

    - added web design (ux, ui)

--- app/controllers/stock_app_controller.rb
+++ app/controllers/stock_app_controller.rb
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-class StockAppController < ApplicationController
-    skip_before_action :verify_authenticity_token
-    before_action :stock_params, only: [:purchase]
-    before_action :authenticate_user! do
-        redirect_to unapproved_path unless current_user.status == 'approved'
-    end
-    
-
-    def index   
-        @markets = Market.all
-    end
-
-    def buy
-        @user = User.find(params[:user_id])
-        @user_id = params[:user_id]
-        @stock_name = params[:name]
-        @stock_ticker = params[:ticker]
-        @stock_price = params[:last_price]               
-    end
-
-    def purchase        
-        @user = User.find(params[:user_id])            
-        @stock = @user.stocks.new(stock_params)
-        if @stock.save
-            redirect_to stockapp_path
-            flash[:notice] = "Stock purchased successfully!"
-        else
-            flash[:notice] = "Stock purchase failed!"
-            render :buy
-        end
-    end
-
-    private
-
-    def stock_params    
-        params.permit(:user_id, :name, :ticker, :last_price, :quantity)
-    end
-=======
 class StockAppController < ApplicationController  
   before_action :stock_params, only: [:purchase, :liquidate]
   before_action :authenticate_user! do
@@ -46,10 +7,12 @@ class StockAppController < ApplicationController
   def index
     @markets = Market.get_most_active  
     @gainers = Market.get_gainers
+    @losers = Market.get_losers
   end
 
   def profile
     @stocks = current_user.stocks
+    # @user = User.find(params[:id])
   end
 
   def buy
@@ -136,5 +99,4 @@ class StockAppController < ApplicationController
   def stock_params    
     params.permit(:user_id, :name, :ticker, :last_price, :quantity, :transaction_type)    
   end
->>>>>>> main
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @markets = Market.all
+    # @markets = Market.all
+    @markets = Market.get_most_active  
+    @gainers = Market.get_gainers
+    @losers = Market.get_losers
   end
 end
