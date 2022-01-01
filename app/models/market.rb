dfe3486a84f36c6754dd3473a559935131808f12
Greg Losa Lobrigo Jr <79005005+greglobrigo@users.noreply.github.com>
2022-01-01 20:27:12 +0800

    Feature stockapp (#2)
    * added project requirements

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,10 +1,27 @@
 class Market < ApplicationRecord
-    def self.new_lookup(ticker_symbol)
-        client = IEX::Api::Client.new(
-          publishable_token: Rails.application.credentials.iex_client[:publishable_token],
-          secret_token: Rails.application.credentials.iex_client[:secret_token],
-          endpoint: Rails.application.credentials.iex_client[:endpoint],
-        )
-        client.price(ticker_symbol)
-      end
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex_client[:publishable_token],
+      secret_token: Rails.application.credentials.iex_client[:secret_token],
+      endpoint: Rails.application.credentials.iex_client[:endpoint],
+    )
+    client.price(ticker_symbol)
+  end
+  def self.get_most_active
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex_client[:publishable_token],
+      secret_token: Rails.application.credentials.iex_client[:secret_token],
+      endpoint: Rails.application.credentials.iex_client[:endpoint],
+    )
+    client.stock_market_list(:mostactive)
+  end
+  def self.get_gainers
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex_client[:publishable_token],
+      secret_token: Rails.application.credentials.iex_client[:secret_token],
+      endpoint: Rails.application.credentials.iex_client[:endpoint],
+    )
+    client.stock_market_list(:gainers)
+  end
+
 end
