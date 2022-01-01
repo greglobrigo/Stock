e8a6fc100dcbf2ad9c30afa4e0df83fb06e8f5f2
Bryan Lontoc <85468571+bryanlontoc06@users.noreply.github.com>
2022-01-01 20:51:56 +0800

    Feature/design (#1)
    * - added bootstrap/jquery/popper
    
    * - added styling in home page, navbar and forms
    
    * - welcome page design in progress
    
    * - added design in welcome page

--- app/controllers/stock_app_controller.rb
+++ app/controllers/stock_app_controller.rb
@@ -1,3 +1,42 @@
+<<<<<<< HEAD
+class StockAppController < ApplicationController
+    skip_before_action :verify_authenticity_token
+    before_action :stock_params, only: [:purchase]
+    before_action :authenticate_user! do
+        redirect_to unapproved_path unless current_user.status == 'approved'
+    end
+    
+
+    def index   
+        @markets = Market.all
+    end
+
+    def buy
+        @user = User.find(params[:user_id])
+        @user_id = params[:user_id]
+        @stock_name = params[:name]
+        @stock_ticker = params[:ticker]
+        @stock_price = params[:last_price]               
+    end
+
+    def purchase        
+        @user = User.find(params[:user_id])            
+        @stock = @user.stocks.new(stock_params)
+        if @stock.save
+            redirect_to stockapp_path
+            flash[:notice] = "Stock purchased successfully!"
+        else
+            flash[:notice] = "Stock purchase failed!"
+            render :buy
+        end
+    end
+
+    private
+
+    def stock_params    
+        params.permit(:user_id, :name, :ticker, :last_price, :quantity)
+    end
+=======
 class StockAppController < ApplicationController  
   before_action :stock_params, only: [:purchase, :liquidate]
   before_action :authenticate_user! do
@@ -97,4 +136,5 @@ class StockAppController < ApplicationController
   def stock_params    
     params.permit(:user_id, :name, :ticker, :last_price, :quantity, :transaction_type)    
   end
+>>>>>>> main
 end
