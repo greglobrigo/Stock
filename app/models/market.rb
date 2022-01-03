99112ec873b69fb86bab607d0de3ed6941247fbd
greglobrigo <golivefun@gmail.com>
2022-01-04 03:55:25 +0800

    try fix API call on heroku

--- app/models/market.rb
+++ app/models/market.rb
@@ -1,33 +1,33 @@
 class Market < ApplicationRecord
-  def self.new_lookup(ticker_symbol)
+  def self.new_lookup(ticker_symbol)        
     client = IEX::Api::Client.new(
-      publishable_token: Rails.application.credentials.iex_client[:publishable_token],
-      secret_token: Rails.application.credentials.iex_client[:secret_token],
-      endpoint: Rails.application.credentials.iex_client[:endpoint],
+      publishable_token: Rails.application.credentials.iex_client[:publishable_token] ? Rails.application.credentials.iex_client[:publishable_token] : ENV['IEX_PUBLISHABLE_TOKEN'],
+      secret_token: Rails.application.credentials.iex_client[:secret_token] ? Rails.application.credentials.iex_client[:secret_token] : ENV['IEX_SECRET_TOKEN'],
+      endpoint: Rails.application.credentials.iex_client[:endpoint] ? Rails.application.credentials.iex_client[:endpoint] : ENV['IEX_ENDPOINT'],
     )
     client.price(ticker_symbol)
   end
   def self.get_most_active
     client = IEX::Api::Client.new(
-      publishable_token: Rails.application.credentials.iex_client[:publishable_token],
-      secret_token: Rails.application.credentials.iex_client[:secret_token],
-      endpoint: Rails.application.credentials.iex_client[:endpoint],
+      publishable_token: Rails.application.credentials.iex_client[:publishable_token] ? Rails.application.credentials.iex_client[:publishable_token] : ENV['IEX_PUBLISHABLE_TOKEN'],
+      secret_token: Rails.application.credentials.iex_client[:secret_token] ? Rails.application.credentials.iex_client[:secret_token] : ENV['IEX_SECRET_TOKEN'],
+      endpoint: Rails.application.credentials.iex_client[:endpoint] ? Rails.application.credentials.iex_client[:endpoint] : ENV['IEX_ENDPOINT'],
     )
     client.stock_market_list(:mostactive)
   end
   def self.get_gainers
     client = IEX::Api::Client.new(
-      publishable_token: Rails.application.credentials.iex_client[:publishable_token],
-      secret_token: Rails.application.credentials.iex_client[:secret_token],
-      endpoint: Rails.application.credentials.iex_client[:endpoint],
+      publishable_token: Rails.application.credentials.iex_client[:publishable_token] ? Rails.application.credentials.iex_client[:publishable_token] : ENV['IEX_PUBLISHABLE_TOKEN'],
+      secret_token: Rails.application.credentials.iex_client[:secret_token] ? Rails.application.credentials.iex_client[:secret_token] : ENV['IEX_SECRET_TOKEN'],
+      endpoint: Rails.application.credentials.iex_client[:endpoint] ? Rails.application.credentials.iex_client[:endpoint] : ENV['IEX_ENDPOINT'],
     )
     client.stock_market_list(:gainers)
   end
   def self.get_losers
     client = IEX::Api::Client.new(
-      publishable_token: Rails.application.credentials.iex_client[:publishable_token],
-      secret_token: Rails.application.credentials.iex_client[:secret_token],
-      endpoint: Rails.application.credentials.iex_client[:endpoint],
+      publishable_token: Rails.application.credentials.iex_client[:publishable_token] ? Rails.application.credentials.iex_client[:publishable_token] : ENV['IEX_PUBLISHABLE_TOKEN'],
+      secret_token: Rails.application.credentials.iex_client[:secret_token] ? Rails.application.credentials.iex_client[:secret_token] : ENV['IEX_SECRET_TOKEN'],
+      endpoint: Rails.application.credentials.iex_client[:endpoint] ? Rails.application.credentials.iex_client[:endpoint] : ENV['IEX_ENDPOINT'],
     )
     client.stock_market_list(:losers)
   end
